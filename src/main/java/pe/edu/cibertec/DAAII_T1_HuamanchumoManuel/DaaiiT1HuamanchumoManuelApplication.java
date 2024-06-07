4f71681808284d67d3848ad2900294f0a19c9fb3
AlexHc26 <mahc2601@gmail.com>
2024-06-07 08:57:48 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_HuamanchumoManuel/DaaiiT1HuamanchumoManuelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_HuamanchumoManuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1HuamanchumoManuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1HuamanchumoManuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAAII_T1_HuamanchumoManuel/DaaiiT1HuamanchumoManuelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_HuamanchumoManuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaaiiT1HuamanchumoManuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
